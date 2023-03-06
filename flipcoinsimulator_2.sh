707c35b2ca84d5e59007231d33da2902a050633a
altaf <altaf98khan66@gmail.com>
2023-03-06 20:44:27 +0530

    ADDED:flip coin simulator

---
+++ flipcoinsimulator_2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+heads=0
+tails=0
+
+while [ $heads -lt 21 ] && [ $tails -lt 21 ]
+do
+flip=$(( RANDOM % 2 ))
+if [ $flip -eq 0 ]
+then
+heads=$(heads+1)
+echo "heads"
+else
+tails=$(tails+1)
+echo "tails"
+fi
+done
+
+if [ $heads -gt $tails ]
+then
+echo "heads won total"
+elif [ $tails -gt $heads ]
+then
+echo "tails won total"
+else
+echo "TIE occurs"
+fi
